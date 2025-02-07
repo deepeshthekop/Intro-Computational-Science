1f4690a15c506d22743802fd609ed19fe1c61956
deepeshthekop <deepeshthekop8@gmail.com>
2025-02-07 16:43:04 -0500

    made updates to quiz_1 q3

--- quiz_1/q3.py
+++ quiz_1/q3.py
@@ -12,7 +12,7 @@ y = (pow(x,3))*(np.exp(-x))
 
 forward_difference = np.diff(y)/h 
 x_diff = x[:-1:]
-exact_derivative = (3*pow(x[:-1],2)*np.exp(-x[:-1])) - (pow(x[:-1],3)*np.exp(-x[:-1]))
+exact_derivative = (3*pow(x_diff,2)*np.exp(-x_diff)) - (pow(x_diff,3)*np.exp(-x_diff))
 
 plt.figure(figsize=(12, 8))
 plt.plot(x_diff, forward_difference, "--", label='Forward Difference Approximation')
