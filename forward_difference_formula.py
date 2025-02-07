02efa0a56431d97f3c2feb12995644aa0e278f74
deepeshthekop <deepeshthekop8@gmail.com>
2025-02-06 23:32:09 -0500

    updated numerical differentiation scripts

--- forward_difference_formula.py
+++ forward_difference_formula.py
@@ -2,19 +2,19 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-# plt.style.use('seaborn-poster')
+#plt.style.use('seaborn-poster')
 
 h = 0.1 # step size
 x = np.arange(0, 2*np.pi, h) # define grid
 y = np.cos(x) # compute function
 
 forward_difference = np.diff(y)/h # compute vector of forward differences
-x_diff = x[:-1] # compute corresponding grid
+x_diff = x[:-1:] # compute corresponding grid
 exact_solution = -np.sin(x_diff) # compute exact solution
 
 #plot solution
 plt.figure(figsize=(12, 8))
-plt.plot(x_diff, forward_difference, "-", label='Finite Difference Approximation')
+plt.plot(x_diff, forward_difference, "--", label='Finite Difference Approximation')
 plt.plot(x_diff, exact_solution, label='Exact Solution')
 plt.legend()
 plt.show()
